678bc5e45c61d2f503cce077165b1b2e577a3ba2
emmanueloghenetegaakponine <emma40tg@gmail.com>
2021-06-22 14:24:34 +0100

    second commit

---
+++ app/src/main/java/com/tega/myapplication/HomeActivity2.kt
@@ -0,0 +1,20 @@
+package com.tega.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.TextureView
+import android.widget.TextView
+
+class HomeActivity2 : AppCompatActivity() {
+
+    private lateinit var welcome1:TextView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home2)
+// finding value by id
+
+        welcome1 = findViewById(R.id.welcome1)
+
+        welcome1.text = "Hello Akponine"
+    }
+}
--- app/src/main/java/com/tega/myapplication/MainActivity.kt
+++ app/src/main/java/com/tega/myapplication/MainActivity.kt
@@ -1,21 +1,48 @@
 package com.tega.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
-    val userName: String = "Admin"
-    val password: String = "Admin"
+    val validUserName: String = "Admin"
+    val validpassword: String = "Admin"
 
-   private lateinit var userNameInput: EditText
+    //layout variables
+    private lateinit var userNameInput: EditText
     private lateinit var passwordInput: EditText
     private lateinit var submitButton: Button
-
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        userNameInput = findViewById(R.id.userNameInput)
+        passwordInput = findViewById(R.id.passwordInput)
+        submitButton = findViewById(R.id.submitButton)
+
+        // submit functionalty
+        submitButton.setOnClickListener {
+            val username = userNameInput.text.toString()
+            val password = passwordInput.text.toString()
+            login(username, password)
+        }
+    }
+
+    fun login(username:String, password:String){
+     if (isvalidcredentials(username,password)){
+         val intent : Intent = Intent(this, HomeActivity2::class.java)
+         intent.putExtra("userName", username)
+         startActivity(intent)
+     }else{
+         Toast.makeText(applicationContext, "incorrect password", Toast.LENGTH_SHORT).show()
+     }
+    }
+    private fun isvalidcredentials(username: String, password: String):Boolean{
+    if(username == validUserName && password == validpassword) return true
+    return false
     }
 }
